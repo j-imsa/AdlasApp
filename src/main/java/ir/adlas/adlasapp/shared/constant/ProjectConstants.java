de112dfd14c2d450be1080ea1e8da082e59be419
Iman Salehi <cse.isalehi@gmail.com>
2025-01-23 08:37:33 +0330

    add basic doc swagger

---
+++ src/main/java/ir/adlas/adlasapp/shared/constant/ProjectConstants.java
@@ -0,0 +1,25 @@
+package ir.adlas.adlasapp.shared.constant;
+
+public class ProjectConstants {
+
+    private ProjectConstants() {}
+
+    // ======================== Application ======================== //
+    public static final String APP_VERSION = "v0.0.1";
+
+    // ======================== API Definition ======================== //
+    public static final String API_DOCUMENT_INFO_TITLE = "Adlas App Document Information";
+    public static final String API_DOCUMENT_INFO_DESCRIPTION = "AdlasApp microservice REST API documentation";
+    public static final String API_DOCUMENT_INFO_VERSION = APP_VERSION;
+    public static final String API_DOCUMENT_INFO_CONTACT_NAME = "Iman Salehi";
+    public static final String API_DOCUMENT_INFO_CONTACT_URL = "https://www.linkedin.com/in/jimsa/";
+    public static final String API_DOCUMENT_INFO_CONTACT_EMAIL = "cse.isalehi@gmail.com";
+    public static final String API_DOCUMENT_INFO_LICENSE_NAME = "MIT License";
+    public static final String API_DOCUMENT_INFO_LICENSE_URL = "https://opensource.org/license/mit";
+    public static final String API_DOCUMENT_EXTERNAL_DOCS_DESCRIPTION = "Source code repository";
+    public static final String API_DOCUMENT_EXTERNAL_DOCS_URL = "https://github.com/j-imsa/AdlasApp.git";
+    public static final String API_DOCUMENT_SERVERS_1_DESCRIPTION = "Dev, local";
+    public static final String API_DOCUMENT_SERVERS_1_URL = "http://localhost:8088/";
+    public static final String API_DOCUMENT_SERVERS_2_DESCRIPTION = "Dev, Server";
+    public static final String API_DOCUMENT_SERVERS_2_URL = "http://152.11.42.185:8090/";
+}
