5d54f3a2b3d2181e3819c4af1686e17962828775
Iman Salehi <cse.isalehi@gmail.com>
2025-01-23 07:58:58 +0330

    init project

---
+++ src/main/java/ir/adlas/adlasapp/AdlasAppApplication.java
@@ -0,0 +1,13 @@
+package ir.adlas.adlasapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdlasAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdlasAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/adlas/adlasapp/AdlasAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.adlas.adlasapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdlasAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
