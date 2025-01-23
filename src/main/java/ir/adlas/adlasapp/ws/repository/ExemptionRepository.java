9a78c1c56bcdb97d8371d64896d78adec4c26f6c
Iman Salehi <cse.isalehi@gmail.com>
2025-01-23 13:05:01 +0330

    add findByPublicId

--- src/main/java/ir/adlas/adlasapp/ws/repository/ExemptionRepository.java
+++ src/main/java/ir/adlas/adlasapp/ws/repository/ExemptionRepository.java
@@ -4,6 +4,9 @@ import ir.adlas.adlasapp.ws.model.entity.ExemptionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ExemptionRepository extends JpaRepository<ExemptionEntity, Long> {
+    Optional<ExemptionEntity> findByPublicId(String publicId);
 }
